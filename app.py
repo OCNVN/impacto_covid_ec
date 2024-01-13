2715bc411e40ef18fcfb2cfba541eca9a980fbb1
Carlos Lucero <nandolucero@icloud.com>
2024-01-13 16:34:20 -0500

    Add Ecuador map with political jurisdictions

--- app.py
+++ app.py
@@ -15,15 +15,12 @@ for feature in geojson['features']:
     print(feature['properties'])
 
 # Prepare some data
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Berries"],
-    "Amount": [4, 1, 2, 2],
-})
+# df = pd.DataFrame({
+#     "Fruit": ["Azuay", "Guayas", "Chimborazo", "Galápagos"],
+#     "Amount": [4, 1, 2, 2],
+# })
 
-# Create the figure using plotly.graph_objects
-fig = go.Figure(data=[
-    go.Bar(name='Fruits', x=df['Fruit'], y=df['Amount'])
-])
+df = pd.read_csv('./data/defunciones_semanal.csv')
 
 # Create the Dash app
 app = dash.Dash(__name__)
@@ -37,12 +34,8 @@ app.layout = html.Div(children=[
     '''),
 
     dcc.Graph(
-        id='example-graph',
-        figure=fig
-    ),
-    
-    dcc.Graph(
         id='example-map',
+        config={'displayModeBar': False},
         # figure=go.Figure(data=go.Choropleth(
         #     geojson=geojson,
         #     locations=["780006B67819952919353"], # This should be a list of locations in your GeoJSON
@@ -53,16 +46,36 @@ app.layout = html.Div(children=[
         #     marker_line_width=0,
         #     showscale=False
         # ))
-        figure=px.choropleth(
+        # figure=px.choropleth(
+        #     geojson=geojson,
+        #     # locations=["780006B67819952919353"], # This should be a list of locations in your GeoJSON
+        #     color=[0], # This should be a list of values for color
+        #     color_continuous_scale="Viridis",
+        #     range_color=(0, 12),
+        #     labels={'color':'Color'}
+        # )
+        figure=px.choropleth_mapbox(df,
             geojson=geojson,
-            # locations=["780006B67819952919353"], # This should be a list of locations in your GeoJSON
-            color=[0], # This should be a list of values for color
+            locations="provincia", # This should be a list of locations in your GeoJSON
+            featureidkey="properties.shapeName",
+            color="semana", # This should be a column in your DataFrame
             color_continuous_scale="Viridis",
             range_color=(0, 12),
-            labels={'color':'Color'}
+            mapbox_style="carto-darkmatter",
+            zoom=5,
+            center = {"lat": -1.8312, "lon": -78.1834},
+            opacity=1,
+            labels={'semana':'Semana'},
+            
         )
-    )
+    ),
+    html.Pre(id='console-output')
 ])
+
+@app.callback(dash.dependencies.Output('console-output', 'children'), [dash.dependencies.Input('example-map', 'clickData')])
+def display_click_data(clickData):
+    return json.dumps(clickData, indent=2)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
